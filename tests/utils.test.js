820734a35b5076a98a2d7fe9d93534b0a402aa27
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2021-07-11 15:50:06 -0700

    2nd challenge done

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -57,6 +57,9 @@ beforeEach((done) => {
   utils.clearCart()
   done()
 })
+  utils.clearCart();
+  done();
+});
 
 it("Should create a new (object) Item with name and price", function() {
   const item = utils.createItem("apple", 0.99)
@@ -65,14 +68,48 @@ it("Should create a new (object) Item with name and price", function() {
   expect(item).to.have.property("price", 0.99)
   expect(item).to.have.property("quantity", 1)
 })
+  const item = utils.createItem("apple", 0.99);
+  expect(item).to.be.a("object");
+  expect(item).to.have.property("name", "apple");
+  expect(item).to.have.property("price", 0.99);
+  expect(item).to.have.property("quantity", 1);
+});
 
-it("Should return an array containing all items in cart")
+it("Should return an array containing all items in cart", function() {
+    const cart = utils.getShoppingCart();
+    expect(cart).to.be.an("array");
+    expect(cart).to.empty;
 
-it("Should add a new item to the shopping cart")
+    const newItem = utils.createItem("apple", 0.99);
+    utils.addItemToCart(newItem);
+    expect(cart).to.be.an("array");
+    expect(cart).to.have.length(1);
+});
 
-it("Should return the number of items in the cart")
+it("Should add a new item to the shopping cart", function() {
+    const newItem = utils.createItem("apple", 0.99);
+    utils.addItemToCart(newItem);
+    const cart = utils.getNumItemsInCart();
+    expect(cart).to.equal(1);
+});
 
-it("Should remove items from cart")
+it("Should return the number of items in the cart", function() {
+    const newItem = utils.createItem("apple", 0.99);
+    utils.addItemToCart(newItem);
+    const cart = utils.getNumItemsInCart();
+    expect(cart).to.equal(1);
+});
+
+it("Should remove items from cart", function() {
+    const newItem = utils.createItem("apple", 0.99);
+    utils.addItemToCart(newItem);
+    var cart = utils.getNumItemsInCart();
+    expect(cart).to.equal(1);
+    utils.removeItemFromCart(newItem);
+
+    cart = utils.getNumItemsInCart();
+    expect(cart).to.equal(0);
+});
 
 // ========================================================
 // Stretch Challenges
